6e2fcee4acd3c29cd1c0d4199f9f65e22b7bf5dc
Alexander Gunnarsson <alexander@workmobile.se>
2024-05-08 09:23:55 +0200

    created xcode project

---
+++ Step tracker/Step_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_trackerApp.swift
+//  Step tracker
+//
+//  Created by Alexander Gunnarsson on 2024-05-08.
+//
+
+import SwiftUI
+
+@main
+struct Step_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
